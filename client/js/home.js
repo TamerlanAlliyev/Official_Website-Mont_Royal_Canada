0d1f5627b3c7569d76762ab6d35396f4dcb89bf5
Tamerlan Aliyev <tamerlanma@code.edu.az>
2025-01-19 16:04:26 +0400

    section: services  finshed

--- client/js/home.js
+++ client/js/home.js
@@ -35,39 +35,89 @@ var swiper = new Swiper(".services .mySwiper", {
     clickable: true,
   },
  
+  // breakpoints: {
+  //   "@0.00": {
+  //     slidesPerView: 1,
+  //     spaceBetween: 20,
+  //     grid: {
+  //       rows: 2,
+  //       fill: "row",
+  //     }
+  //   },
+  //   "@0.75": {
+  //     slidesPerView: 2,
+  //     spaceBetween: 30,
+  //     grid: {
+  //       rows: 2,
+  //       fill: "row",
+  //     }
+  //   }, 768: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20,
+  //     navigation: {
+  //       nextEl: '.swiper-button-next',
+  //       prevEl: '.swiper-button-prev',
+  //     },
+  //   },
+  //   "@1.00": {
+  //     slidesPerView: 2,
+  //     spaceBetween: 40,
+  //     grid: {
+  //       rows: 2,
+  //       fill: "row",
+  //     }
+  //   },
+  //   "@1.50": {
+  //     slidesPerView: 4,
+  //     spaceBetween: 50,
+  //     grid: {
+  //       rows: 2,
+  //       fill: "row",
+  //     }
+  //   },
+  // },
+
   breakpoints: {
-    "@0.00": {
+    320: {
       slidesPerView: 1,
+      spaceBetween: 10,
+      grid: {
+        rows: 2, 
+        fill: 'row', 
+      },
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      grid: {
+        rows: 2, 
+        fill: 'row', 
+      },
+    },
+    768: {
+      slidesPerView: 3,
       spaceBetween: 20,
       grid: {
-        rows: 2,
-        fill: "row",
-      }
+        rows: 2, 
+        fill: 'row', 
+      },
     },
-    "@0.75": {
-      slidesPerView: 2,
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+      grid: {
+        rows: 2, 
+        fill: 'row', 
+      },
+    },
+    1440: {
+      slidesPerView: 4,
       spaceBetween: 30,
       grid: {
-        rows: 2,
-        fill: "row",
-      }
+        rows: 2, 
+        fill: 'row', 
+      },
     },
-    "@1.00": {
-      slidesPerView: 3,
-      spaceBetween: 40,
-      grid: {
-        rows: 2,
-        fill: "row",
-      }
-    },
-    "@1.50": {
-      slidesPerView: 4,
-      spaceBetween: 50,
-      grid: {
-        rows: 2,
-        fill: "row",
-      }
-    },
-  },
+  }
 });
 
